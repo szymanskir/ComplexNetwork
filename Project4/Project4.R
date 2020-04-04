37f5dab602bd7197d605b4c43075ca5083302eb2
szymanskir <ryszard.szymanski@outlook.com>
2020-04-04 21:21:34 +0200

    Add clustering coefficient measures

--- Project4/Project4.R
+++ Project4/Project4.R
@@ -27,6 +27,35 @@ capability <- function(graph) {
   sum(vertex_distances_inverted) / (vertex_count * (vertex_count - 1))
 }
 
+clustering_coefficient_1 <- function(graph) {
+  node_clustering_coefficient <- function(graph, v) {
+    v_degree <- degree(graph, v)
+    v_neighbors <- neighbors(graph, v) %>% as.numeric()
+    neighborhood_edges_count <- as_adjacency_matrix(graph)[v_neighbors, v_neighbors] %>% 
+      sum() / 2
+    possible_edges_count <- choose(v_degree, 2)
+    
+    clustering_coefficient <- neighborhood_edges_count / possible_edges_count
+    if (is.nan(clustering_coefficient)) {
+      NA
+    } else {
+      clustering_coefficient
+    }
+  }
+  
+  mean(sapply(V(graph), node_clustering_coefficient, graph = graph), na.rm = TRUE)
+}
+
+clustering_coefficient_2 <- function(graph) {
+  triangles_count <- length(triangles(graph)) / 3
+  all_simple_paths_of_legth_2_count <- do.call(c, lapply(V(graph), all_simple_paths, graph = graph)) %>% 
+    keep(function(path) length(path) == 3) %>% 
+    unique() %>% 
+    length() / 2
+  
+  (3 * triangles_count) / all_simple_paths_of_legth_2_count
+}
+
 node_betweenness <- function(graph) {
   single_node_betweenness <- function(graph, v) {
     start_nodes <- setdiff(V(graph), v)
